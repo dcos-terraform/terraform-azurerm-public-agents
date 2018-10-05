840ed8d3b94b9326fa9cbae721d514b61246e7bb
Jan Ulferts <julferts@mesosphere.com>
2018-10-05 12:47:32 +0200

    cleanup outputs

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
 output "admin_username" {
   description = "admin username"
-  value       = "${module.dcos-pubagt-instances.admin_username}"
+  value       = "${module.dcos-public-agent-instances.admin_username}"
 }
 
 # Private IP Addresses
 output "private_ips" {
   description = "List of private ip addresses created by this module"
-  value       = ["${module.dcos-pubagt-instances.private_ips}"]
+  value       = ["${module.dcos-public-agent-instances.private_ips}"]
 }
 
 # Public IP Addresses
 output "public_ips" {
   description = "List of public ip addresses created by this module"
-  value       = ["${module.dcos-pubagt-instances.public_ips}"]
+  value       = ["${module.dcos-public-agent-instances.public_ips}"]
 }
 
 # Returns the ID of the prereq script
 output "prereq_id" {
   description = "prereq id"
-  value       = "${module.dcos-pubagt-instances.prereq_id}"
+  value       = "${module.dcos-public-agent-instances.prereq_id}"
 }
