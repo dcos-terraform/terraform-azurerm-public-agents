c66e5f5569060172fc90abf46674ef1759ea0f52
Weston Bassler <wbassler@mesosphere.com>
2018-10-23 16:55:06 -0400

    update docs

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,19 @@
 output "admin_username" {
-  description = "admin username"
+  description = "SSH User"
   value       = "${module.dcos-public-agent-instances.admin_username}"
 }
 
-# Private IP Addresses
 output "private_ips" {
   description = "List of private ip addresses created by this module"
   value       = ["${module.dcos-public-agent-instances.private_ips}"]
 }
 
-# Public IP Addresses
 output "public_ips" {
   description = "List of public ip addresses created by this module"
   value       = ["${module.dcos-public-agent-instances.public_ips}"]
 }
 
-# Returns the ID of the prereq script
 output "prereq_id" {
-  description = "prereq id"
+  description = "Prereq id used for dependency"
   value       = "${module.dcos-public-agent-instances.prereq_id}"
 }
