6d1941848634ed36ffb62df9ca61fbb27a735d81
Jan Ulferts <julferts@mesosphere.com>
2020-06-11 14:18:25 +0200

    feat: upgrade to tf 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "admin_username" {
   description = "SSH User"
-  value       = "${module.dcos-public-agent-instances.admin_username}"
+  value       = module.dcos-public-agent-instances.admin_username
 }
 
 output "private_ips" {
   description = "List of private ip addresses created by this module"
-  value       = ["${module.dcos-public-agent-instances.private_ips}"]
+  value       = module.dcos-public-agent-instances.private_ips
 }
 
 output "public_ips" {
   description = "List of public ip addresses created by this module"
-  value       = ["${module.dcos-public-agent-instances.public_ips}"]
+  value       = module.dcos-public-agent-instances.public_ips
 }
 
 output "instance_nic_ids" {
   description = "List of instance nic ids created by this module"
-  value       = ["${module.dcos-public-agent-instances.instance_nic_ids}"]
+  value       = module.dcos-public-agent-instances.instance_nic_ids
 }
